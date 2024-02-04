abbcb4bfed16a654bcd56a153c5ac7387d033f63
Kes <67107510+AceOfKestrels@users.noreply.github.com>
2024-02-04 03:38:49 +0100

    Initial commit

---
+++ Project/Services/RepoService.cs
@@ -0,0 +1,20 @@
+﻿namespace Kestrels_Wiki.Services;
+public class RepoService
+{
+    public string Content { get; private set; }
+
+    public async void FetchContent()
+    {
+        HttpClient client = new();
+        client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.raw+json");
+        client.DefaultRequestHeaders.Add("User-Agent", "AceOfKestrels");
+        var response = await client.GetAsync("https://api.github.com/repos/AceOfKestrels/Brain/git/blobs/310386b456c2400bfd85716d153c9a3a6e34fc5d");
+
+        using StreamReader reader = new(response.Content.ReadAsStream());
+        Content = reader.ReadToEnd().Replace("<", "&lt;");
+
+        using StreamWriter writer = new(File.OpenWrite("./wwwroot/test.md"));
+        writer.Write(Content);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kestrels-Wiki
+ASP.NET Core Blazor based web application to render and enable searching of my brain in the style of a wiki.
